e52583419826a8c0d9051ceccde632cb80bfd7fc
Adam Beswick <adam.beswick@bbc.co.uk>
2018-02-08 16:01:57 +0000

    enables asset loader tests

--- test/core/asset-loader.spec.ts
+++ test/core/asset-loader.spec.ts
@@ -14,7 +14,7 @@ describe("Asset Loader", () => {
     beforeEach(installMockGetGmi);
     afterEach(uninstallMockGetGmi);
 
-    xit("Should callback with 100% progress when 0 files are to be loaded in gamePacks.", () => {
+    it("Should callback with 100% progress when 0 files are to be loaded in gamePacks.", () => {
         const updateCallback = sinon.spy();
         const gamePacks: PackList = {
             MASTER_PACK_KEY: { url: assetPacks.emptyAssetPack },
@@ -34,7 +34,7 @@ describe("Asset Loader", () => {
             });
     });
 
-    xit("Should be called 4 times (at 25% intervals) when 4 files are to be loaded in gamePacks.", () => {
+    it("Should be called 4 times (at 25% intervals) when 4 files are to be loaded in gamePacks.", () => {
         const updateCallback = sinon.spy();
         const gamePacks: PackList = {
             MASTER_PACK_KEY: { url: assetPacks.twoScreensThreeAssetsPack },
@@ -59,7 +59,7 @@ describe("Asset Loader", () => {
             });
     });
 
-    xit("Should resolve the returned Promise with keyLookups for each gamePack screen.", () => {
+    it("Should resolve the returned Promise with keyLookups for each gamePack screen.", () => {
         const updateCallback = sinon.spy();
         const gamePacks: PackList = {
             MASTER_PACK_KEY: { url: assetPacks.twoScreensThreeAssetsPack },
@@ -81,7 +81,7 @@ describe("Asset Loader", () => {
             });
     });
 
-    xit("Should correctly namespace assets by their URL and return it in keyLookups.", () => {
+    it("Should correctly namespace assets by their URL and return it in keyLookups.", () => {
         const updateCallback = sinon.spy();
         const gamePacks: PackList = {
             MASTER_PACK_KEY: { url: assetPacks.oneScreenOneAssetPack },
@@ -102,7 +102,7 @@ describe("Asset Loader", () => {
             });
     });
 
-    xit("Should attempt to load assetPack JSON files that are missing and include them in keyLookups", () => {
+    it("Should attempt to load assetPack JSON files that are missing and include them in keyLookups", () => {
         const updateCallback = sinon.spy();
         const loadSpy = sinon.spy();
         const getJSONStub = sinon.stub(Phaser.Cache, "JSON").callsFake((key: string, clone?: boolean | undefined) => {
