bcce3f083699d661046d2b37d93e9a33087b9bae
Nick Tipping <nick.tipping@bbc.co.uk>
2018-03-02 11:31:11 +0000

    Remove describe.only

--- src/core/signal-bus.ts
+++ src/core/signal-bus.ts
@@ -1,16 +1,32 @@
 /*
 
-    addSignal(name: string);
+    X addSignal(name: string);
     removeSignal(name: string)
-    publish(name: string, data: any)
-    subscribe:(name)
+    X publish(name: string, data: any)
+    X subscribe:(name)
 
     checks:
         * On subscribing to a signal is it the callback fired?
         * On publishing a signal is the data payload sent?
         * Signals use unique names
 */
+export const create = () => {
+    const bus = {};
 
+    const add = name => {
+        if (bus[name]) {
+            throw new Error("Signal already exists");
+        }
+        bus[name] = new Phaser.Signal();
+    };
 
+    const remove = name => {
+        bus[name].dispose();
+        delete bus[name];
+    };
 
+    const subscribe = (callback, name) => bus[name].add(callback);
+    const publish = (name, data?) => bus[name].dispatch(data);
 
+    return { add, remove, subscribe, publish };
+};
--- test/core/signal-bus.spec.ts
+++ test/core/signal-bus.spec.ts
@@ -1,13 +1,48 @@
 import "../../src/lib/phaser";
 
 import { assert } from "chai";
+import * as sinon from "sinon";
+
+import * as SignalBus from "../../src/core/signal-bus";
+
+       // * On subscribing to a signal is it the callback fired?
+       // * On publishing a signal is the data payload sent?
+       // * Signals use unique names
 
 
-describe("Signal Bus", () => {
+describe.only("Signal Bus", () => {
 
-    it("Should callback with 100% progress when 0 files are to be loaded in gamePacks.", () => {
+    it("Should fire the correct callbacks when a signal is published", () => {
+        const bus = SignalBus.create();
 
+        bus.add("testSignal1");
+        bus.add("testSignal2");
 
+        const callback1 = sinon.spy();
+        const callback2 = sinon.spy();
+        const callback3 = sinon.spy();
+
+        bus.subscribe(callback1, "testSignal1");
+        bus.subscribe(callback2, "testSignal1");
+        bus.subscribe(callback3, "testSignal2");
+
+        bus.publish("testSignal1");
+        bus.publish("testSignal1", 12345);
+        bus.publish("testSignal1", {exampleData: "abcdef"});
+
+        bus.publish("testSignal2", {aa: "bb"});
+        bus.publish("testSignal2", {aa: "bb"});
+
+        assert(callback1.callCount === 3);
+        assert(callback2.callCount === 3);
+        assert(callback3.callCount === 2);
+    });
+
+    it("Should only allow unique signal names", () => {
+        const bus = SignalBus.create();
+
+        bus.add("testSignal1");
+        bus.add("testSignal1");
     });
 });
 
@@ -15,21 +50,21 @@ describe("Signal Bus", () => {
  * Wraps a test in asynchronous Phaser setup and shutdown code, and runs it in the preload phase of the first state.
  * @param action Function to run the tests, returning a promise.
  */
-function runInPreload(action: (g: Phaser.Game) => Promise<void>): Promise<void> {
-    const promisedTest = new PromiseTrigger<void>();
-    const testState = new class extends Screen {
-        public preload() {
-            promisedTest.resolve(action(this.game));
-        }
-    }();
-    const transitions = [
-        {
-            name: "loadscreen",
-            state: testState,
-            nextScreenName: () => "loadscreen",
-        },
-    ];
-    return startup(transitions)
-        .then(game => promisedTest.then(() => game))
-        .then(game => game.destroy());
-}
+// function runInPreload(action: (g: Phaser.Game) => Promise<void>): Promise<void> {
+//     const promisedTest = new PromiseTrigger<void>();
+//     const testState = new class extends Screen {
+//         public preload() {
+//             promisedTest.resolve(action(this.game));
+//         }
+//     }();
+//     const transitions = [
+//         {
+//             name: "loadscreen",
+//             state: testState,
+//             nextScreenName: () => "loadscreen",
+//         },
+//     ];
+//     return startup(transitions)
+//         .then(game => promisedTest.then(() => game))
+//         .then(game => game.destroy());
+// }
