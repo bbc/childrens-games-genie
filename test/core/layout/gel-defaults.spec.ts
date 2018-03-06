6f0f7d96d930248f8731ff6c4ec92a1427283587
Thomas-James-Rose <tom.rose@bbc.co.uk>
2018-03-06 15:00:07 +0000

    Added missing semi-colons

--- test/core/layout/gel-defaults.spec.ts
+++ test/core/layout/gel-defaults.spec.ts
@@ -4,19 +4,19 @@ import config from "../../../src/core/layout/gel-defaults";
 describe("Gel Defaults", () => {
 
     it("has an exit button with title 'Exit'", () => {
-        assert(config.exit.title === "Exit")
+        assert(config.exit.title === "Exit");
     });
 
     it("has a play button with group 'middleCenterV'", () => {
-        assert(config.play.group === "middleCenterV")
+        assert(config.play.group === "middleCenterV");
     });
 
     it("has a settings button with aria label 'Game Settings'", () => {
-        assert(config.settings.ariaLabel === "Game Settings")
+        assert(config.settings.ariaLabel === "Game Settings");
     });
 
     it("has a home button with key 'home'", () => {
-        assert(config.home.key === "home")
+        assert(config.home.key === "home");
     });
 
 });
