4bdc6c8b08a3bd718f23160a376c97839242bac5
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-03-16 15:28:20 +0000

    Prettify calculate-metrics

--- src/core/layout/calculate-metrics.js
+++ src/core/layout/calculate-metrics.js
@@ -2,12 +2,7 @@ const BORDER_PAD = 0.02;
 const BREAK_WIDTH = 770;
 const SAFE_ASPECT_RATIO = 4 / 3;
 
-export const calculateMetrics = (
-    width,
-    height,
-    scale,
-    stageHeight,
-) => {
+export const calculateMetrics = (width, height, scale, stageHeight) => {
     const isMobile = width < BREAK_WIDTH;
     const safeWidth = height * SAFE_ASPECT_RATIO;
 
@@ -23,18 +18,18 @@ export const calculateMetrics = (
         horizontals: {
             left: width / scale * -0.5,
             center: 0,
-            right: width / scale * 0.5,
+            right: width / scale * 0.5
         },
         safeHorizontals: {
             left: safeWidth * -0.5,
             center: 0,
-            right: safeWidth * 0.5,
+            right: safeWidth * 0.5
         },
         verticals: {
             top: stageHeight * -0.5,
             middle: 0,
-            bottom: stageHeight * 0.5,
-        },
+            bottom: stageHeight * 0.5
+        }
     };
 
     return metrics;
