5daa2db24909b560fbf02ab1ae9abc29d2df75e0
Thomas-James-Rose <Thomas-James-Rose@users.noreply.github.com>
2018-03-21 17:14:45 +0000

    Fix typo

--- tag-release.sh
+++ tag-release.sh
@@ -4,5 +4,5 @@ set -e
 VERSION_BUMP_TYPE=$1
 git checkout master; git fetch; git pull; git reset --hard
 git checkout -b new-package-version
-npm version $VERSION_BUMP_TYPE -m "Bumped to version $s"
+npm version $VERSION_BUMP_TYPE -m "Bumped to version %s"
 git push
