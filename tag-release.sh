c73900861b4f52c8255e2faf6af1a356af38e6d7
Thomas-James-Rose <Thomas-James-Rose@users.noreply.github.com>
2018-03-21 17:08:24 +0000

    [CGPROD-476] Branch when version bumping to get around protected master branch

--- tag-release.sh
+++ tag-release.sh
@@ -3,5 +3,6 @@ set -e
 
 VERSION_BUMP_TYPE=$1
 git checkout master; git fetch; git pull; git reset --hard
+git checkout -b new-package-version
 npm version $VERSION_BUMP_TYPE -m "Bumped to version $s"
 git push
