b6bdd7fcf307766fe6397663c3241806deeba6c7
Thomas-James-Rose <Thomas-James-Rose@users.noreply.github.com>
2018-03-22 12:24:15 +0000

    [CGPROD-476] Started adding support for version release notes and added checks for arguments to tag-release.sh

--- tag-release.sh
+++ tag-release.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -z "$1" ] # TODO Validate version bump type
+  then
+    echo "ERROR: Please provide a version type of either patch, minor or major"
+fi
+
+if [ -z "$2" ]
+  then
+    echo "ERROR: Please provide a version description"
+fi
+
 VERSION_BUMP_TYPE=$1
+RELEASE_NOTES=$2
+
+# Bump package.json version and git tag
 git checkout master
-git fetch
 git pull
-git reset --hard
 git checkout -b new-package-version
-npm version $VERSION_BUMP_TYPE -m "Bumped to version %s"
+npm version $VERSION_BUMP_TYPE -m "Bumped to v%s"
+
+# Create the release notes
+NEW_PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+printf "%s\n" "# GENIE v"$NEW_PACKAGE_VERSION $RELEASE_NOTES > ./release-notes.md
+git add ./release-notes.md
+git commit -m "Updated release notes for v"$NEW_PACKAGE_VERSION
+
 git push
