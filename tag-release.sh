e7d99b3fe4a9cc3329319b76ce6ca019d9b42ab8
Thomas-James-Rose <Thomas-James-Rose@users.noreply.github.com>
2018-03-22 10:03:59 +0000

    Formatted tag-release.sh

--- tag-release.sh
+++ tag-release.sh
@@ -2,7 +2,10 @@
 set -e
 
 VERSION_BUMP_TYPE=$1
-git checkout master; git fetch; git pull; git reset --hard
+git checkout master
+git fetch
+git pull
+git reset --hard
 git checkout -b new-package-version
 npm version $VERSION_BUMP_TYPE -m "Bumped to version %s"
 git push
