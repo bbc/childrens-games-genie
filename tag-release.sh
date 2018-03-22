cf19dde71b5a11c9e41202b9db3f0d5cd23b2a62
Thomas-James-Rose <Thomas-James-Rose@users.noreply.github.com>
2018-03-22 14:10:40 +0000

    [CGPROD-476] Improve error catching in tag-release.sh

--- tag-release.sh
+++ tag-release.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$1" ] # TODO Validate version bump type
+if [ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ]
   then
-    echo "ERROR: Please provide a version type of either patch, minor or major"
+    echo -e "\033[0;31mERROR: Please provide a version type of either patch, minor or major\033[0m"
+    exit
 fi
 
 if [ -z "$2" ]
   then
-    echo "ERROR: Please provide a version description"
+    echo -e "\033[0;31mERROR: Please provide a version description\033[0m"
+    exit
+fi
+
+if [ `git branch --list new-package-version` ]
+then
+  while true; do
+    echo "The branch new-package-version already exists locally. Would you like to delete this branch?"
+    read yn
+    case $yn in
+      [Yy]* )
+        git branch -D new-package-version
+        break;;
+      [Nn]* )
+        echo -e "\033[0;31mERROR: Branch new-package-version already exists. Please delete this branch before trying again.\033[0m"
+        exit;;
+      * ) 
+        echo "Please answer yes or no.";;
+    esac
+  done
 fi
 
 VERSION_BUMP_TYPE=$1
@@ -22,7 +42,7 @@ npm version $VERSION_BUMP_TYPE -m "Bumped to v%s"
 
 # Create the release notes
 NEW_PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-printf "%s\n" "# GENIE v"$NEW_PACKAGE_VERSION $RELEASE_NOTES > ./release-notes.md
+printf "%s\n" "# GENIE v${NEW_PACKAGE_VERSION} Release Notes" $RELEASE_NOTES > ./release-notes.md
 git add ./release-notes.md
 git commit -m "Updated release notes for v"$NEW_PACKAGE_VERSION
 
