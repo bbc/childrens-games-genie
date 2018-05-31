258d07db4f9e8f71fc1a5bb32f3144f82f9849af
Sean Holden <sean.holden@bbc.co.uk>
2018-05-31 16:58:50 +0100

    Made a start on new layout scaling

--- src/components/home.js
+++ src/components/home.js
@@ -8,40 +8,32 @@ import * as signal from "../core/signal-bus.js";
 import { createTestHarnessDisplay } from "./test-harness/layout-harness.js";
 import fp from "../../lib/lodash/lodash.js";
 
-const PlayButton = {
-    Draw: (game, x, y) => {
-        const button = game.add.sprite(x, y, "gelDesktop.play");
-        button.anchor.setTo(0.5, 0.5);
-
-        return button;
-    },
-
-    Redraw: (button, x, y) => {
-        if (button) {
-            button.x = x;
-            button.y = y;
-        }
-
-        return button;
-    }
-}
-
 export class Home extends Screen {
     constructor() {
         super();
     }
 
     create() {
+        this.root = this.game.add.group();
+
         window.onresize = fp.debounce(this.resize.bind(this), 200);
-        this.background = this.game.add.image(this.game.world.centerX, this.game.world.centerY, "home.background");
+
+        this.background = this.game.add.image(0, 0, "home.background");
         this.background.anchor.setTo(0.5, 0.5);
-        this.title = this.game.add.image(this.game.world.centerX, this.game.world.centerY - 150, this.getAsset("title"));
+
+        this.title = this.game.add.image(0, -150, this.getAsset("title"));
         this.title.anchor.setTo(0.5, 0.5);
-        this.scene.addLayout(["exit", "howToPlay", "play", "audioOff", "settings"]);
+        //this.scene.addLayout(["exit", "howToPlay", "play", "audioOff", "settings"]);
+
         createTestHarnessDisplay(this.game, this.context, this.scene);
         this.resize();
 
-        //this.button = PlayButton.Draw(this.game, this.game.world.centerX, this.game.world.centerY);
+        this.button = this.game.add.sprite(0, 0, "gelDesktop.play");
+        this.button.anchor.setTo(0.5, 0.5);
+        this.root.add(this.background);
+        this.root.add(this.title);
+        this.root.add(this.button);
+        this.root.position.set(this.game.world.centerX, this.game.world.centerY);
 
         signal.bus.subscribe({
             channel: "gel-buttons",
@@ -63,10 +55,9 @@ export class Home extends Screen {
             this.game.scale.setGameSize(aspectRatio * 600, 600);
         }
 
-        this.background.x = this.game.world.centerX;
-        this.background.y = this.game.world.centerY;
-        this.title.x = this.game.world.centerX;
-        this.title.y = this.game.world.centerY - 150;
-        //PlayButton.Redraw(this.button, this.game.world.centerX, this.game.world.centerY);
+        this.root.position.set(this.game.world.centerX, this.game.world.centerY);
+        if (this.button) {
+            this.button.scale.setTo(600 / window.innerHeight);
+        }
     }
 }
