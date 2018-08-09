647f26254cd15f8aa2adde8d6717841ee2f53e98
Nick Tipping <nick.tipping@bbc.co.uk>
2018-08-09 16:04:52 +0100

    Tidy up

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -1,3 +1,9 @@
+/**
+ * Local rule sets for Genie.
+ *
+ * @example To override the disallow timers rule:
+ *      // eslint-disable-next-line local-rules/disallow-timers
+ */
 module.exports = {
     "disallow-timers": {
         meta: {
@@ -8,19 +14,17 @@ module.exports = {
             },
             schema: [],
         },
-        create: function(context) {
-            return {
-                CallExpression: function(node) {
-                    if (!["setTimeout", "setInterval"].includes(node.callee.name)) {
-                        return;
-                    }
+        create: context => ({
+            CallExpression: node => {
+                if (!["setTimeout", "setInterval"].includes(node.callee.name)) {
+                    return;
+                }
 
-                    context.report({
-                        message: "Phaser timers should be used instead of setTimeout/setInterval to maintain pause functionality",
-                        node: node,
-                    });
-                },
-            };
-        },
+                context.report({
+                    message: "To maintain pause functionality Phaser timers should be used over setTimeout/setInterval",
+                    node: node,
+                });
+            },
+        }),
     },
 };
