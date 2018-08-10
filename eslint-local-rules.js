31acd73723ed54fb7f67a0b40cdf2a3a3445135b
Nick Tipping <nick.tipping@bbc.co.uk>
2018-08-10 14:47:16 +0100

    strip rules from local-rules file

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -2,5 +2,32 @@
  * Rule loading shim for genie specific linting
  * See ./dev/eslint-rules/eslint-rules-genie for available rules
  */
-const genieRules = require("./dev/eslint-rules/eslint-rules-genie");
+//const genieRules = require("./dev/eslint-rules/eslint-rules-genie");
+
+
+const genieRules = {
+    "disallow-timers": {
+        meta: {
+            docs: {
+                description: "Disallow setTimeout",
+                category: "Genie Restrictions",
+                recommended: true,
+            },
+            schema: [],
+        },
+        create: context => ({
+            CallExpression: node => {
+                if (!["setTimeout", "setInterval"].includes(node.callee.name)) {
+                    return;
+                }
+
+                context.report({
+                    message: "To maintain pause functionality Phaser timers should be used over setTimeout/setInterval",
+                    node: node,
+                });
+            },
+        }),
+    },
+};
+
 module.exports = genieRules;
