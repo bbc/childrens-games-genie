056da320987a0657a9bc4a4d5caa85d2a09aed9a
Nick Tipping <nick.tipping@bbc.co.uk>
2019-06-20 09:30:10 +0100

    Prettier fixes

--- build-scripts/webpack.config.js
+++ build-scripts/webpack.config.js
@@ -11,7 +11,7 @@ var phaserModule = path.resolve("node_modules/phaser-ce/");
 var phaser = path.join(phaserModule, "build/custom/phaser-split.js");
 var pixi = path.join(phaserModule, "build/custom/pixi.js");
 var p2 = path.join(phaserModule, "build/custom/p2.js");
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = env => {
     var development = env && env.development;
@@ -64,12 +64,12 @@ module.exports = env => {
 
     webPackConfig.entry.push(path.resolve("src/main.js"));
 
-    const genieVersion = require("../package.json").version
-    webPackConfig.plugins.push(new webpack.BannerPlugin(`\nBBC GAMES GENIE: ${genieVersion}\n`))
+    const genieVersion = require("../package.json").version;
+    webPackConfig.plugins.push(new webpack.BannerPlugin(`\nBBC GAMES GENIE: ${genieVersion}\n`));
 
     if (env && env.genieCore) {
         const Visualizer = require("webpack-visualizer-plugin");
-        webPackConfig.plugins.push( new Visualizer());
+        webPackConfig.plugins.push(new Visualizer());
 
         webPackConfig.devServer.historyApiFallback.index = "dev/index.main.html";
         webPackConfig.devServer.historyApiFallback.rewrites = [
