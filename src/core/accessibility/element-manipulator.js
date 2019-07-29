df97d3ba11a44765dcc3b94362cce7ed1f3608bd
Nick Tipping <nick.tipping@bbc.co.uk>
2019-07-29 14:11:54 +0100

    -manipulator.js          |      100 |

--- src/core/accessibility/element-manipulator.js
+++ src/core/accessibility/element-manipulator.js
@@ -41,11 +41,19 @@ export const hideAndDisableElement = el => {
     }
 };
 
+const nextTick = fn => setTimeout(fn, 0);
+
 const resetElementToDefault = (el, self) => {
     el.removeEventListener("blur", self);
     el.parentElement.removeChild(el);
     showElement(el);
     unsetElementAsHiddenAndDisabled(el);
+
+    nextTick(() => {
+        el.parentElement.removeChild(el);
+        showElement(el);
+        unsetElementAsHiddenAndDisabled(el);
+    });
 };
 
 const elementHiddenAndDisabled = element => {
