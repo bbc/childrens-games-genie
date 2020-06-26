a8ad3b528a15880015cf7f188d00b5ba4573a56a
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2020-06-26 12:38:23 +0100

    Updates coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -7,18 +7,30 @@ module.exports = {
     collectCoverageFrom: ["src/**/*.js", "!src/components/test-harness/**/*.js", "!src/output/**/*.js"],
     coverageThreshold: {
         global: {
-            statements: 94.34,
-            branches: 87.74,
-            lines: 95.25,
-            functions: 90.8,
+            statements: 97.71,
+            branches: 90.66,
+            lines: 97.98,
+            functions: 96.39,
         },
         "src/core/layout/gel-button.js": {
+            statements: 9.09,
+            branches: 0,
+            lines: 10.26,
+            functions: 0,
+        },
+        "src/core/layout/debug-button.js": {
             statements: 0,
             branches: 0,
             lines: 0,
             functions: 0,
         },
-        "src/core/layout/debug-button.js": {
+        "src/core/startup.js": {
+            statements: 67.74,
+            branches: 68.42,
+            lines: 71.19,
+            functions: 41.18,
+        },
+        "src/main.js": {
             statements: 0,
             branches: 0,
             lines: 0,
